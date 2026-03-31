36317418a120aea0801ff5f5716bdec278d6fc60
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2026-03-31 14:42:52 +0900

    feat: image remotePatterns 보강

--- next.config.ts
+++ next.config.ts
@@ -12,14 +12,16 @@ const nextConfig: NextConfig = {
   },
 
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: process.env.NEXT_PUBLIC_PROJECT_LOGO_S3_URL || '',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    remotePatterns: process.env.NEXT_PUBLIC_PROJECT_LOGO_S3_URL
+      ? [
+          {
+            protocol: 'https',
+            hostname: process.env.NEXT_PUBLIC_PROJECT_LOGO_S3_URL,
+            port: '',
+            pathname: '/**',
+          },
+        ]
+      : [],
   },
 
   async rewrites() {
