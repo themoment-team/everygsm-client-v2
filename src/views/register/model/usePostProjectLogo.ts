5c365c7643192c26def71407f37eaef107893f8c
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2026-04-06 08:04:48 +0900

    feat: 이미지 등록 쿼리키 분리

--- src/views/register/model/usePostProjectLogo.ts
+++ src/views/register/model/usePostProjectLogo.ts
@@ -1,7 +1,7 @@
 import { useMutation, UseMutationOptions } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 
-import { imageUrl, post, projectQueryKeys } from '@/shared/api';
+import { imageQueryKeys, imageUrl, post } from '@/shared/api';
 
 import { PostProjectLogoResponse } from './types';
 
@@ -12,7 +12,7 @@ export const usePostProjectLogo = (
   >,
 ) =>
   useMutation({
-    mutationKey: projectQueryKeys.postProjectLogo(),
+    mutationKey: imageQueryKeys.postImageUpload(),
     mutationFn: (data: FormData) => post<PostProjectLogoResponse>(imageUrl.postImageUpload(), data),
     ...options,
   });
