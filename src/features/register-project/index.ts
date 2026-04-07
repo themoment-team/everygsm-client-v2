3bde14e55255bde35f5b12d2e2d09a09fc575a85
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2026-04-07 16:52:06 +0900

    refactor: fsd 구조에 알맞게 구조 변경

---
+++ src/features/register-project/index.ts
@@ -0,0 +1,4 @@
+export * from './model/schema';
+export * from './model/usePostProjectLogo';
+export * from './model/usePostProjectRegistration';
+export { default as RegisterProjectForm } from './ui/RegisterProjectForm';
--- src/widgets/register/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './model/schema';
-export * from './style/textStyle';
-export { default as InputForm } from './ui/InputForm';
