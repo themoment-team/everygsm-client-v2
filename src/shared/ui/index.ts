15ec352ff5b849deebfef49b6965996c09320cfd
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2026-04-08 10:03:41 +0900

    feat: inputForm shared로 이동

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as AppToaster } from './AppToaster';
+export { default as InputForm } from './InputForm';
 export { default as ModalContainer } from './ModalContainer';
 export { default as SuspenseFallback } from './SuspenseFallback';
