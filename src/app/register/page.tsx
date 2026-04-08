8bf0083437ae668ed6d7cdedb9297c4938425537
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2026-04-08 09:26:15 +0900

    feat: 불필요한 Suspense 제거

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,14 +1,7 @@
-import { Suspense } from 'react';
-
-import { SuspenseFallback } from '@/shared/ui';
 import { RegisterPage } from '@/views/register';
 
 const Register = () => {
-  return (
-    <Suspense fallback={<SuspenseFallback />}>
-      <RegisterPage />
-    </Suspense>
-  );
+  return <RegisterPage />;
 };
 
 export default Register;
