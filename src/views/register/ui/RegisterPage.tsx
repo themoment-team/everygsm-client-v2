0629fc00ef66b488255bb33f00b53734493cbfac
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2026-04-13 15:01:17 +0900

    feat: HeroSection 컴포넌트 사용

--- src/views/register/ui/RegisterPage.tsx
+++ src/views/register/ui/RegisterPage.tsx
@@ -1,17 +1,12 @@
 import { RegisterProjectForm } from '@/features/register-project';
 import { cn } from '@/shared/utils';
+import { HeroSection } from '@/widgets/hero-section';
 
 const RegisterPage = () => {
   return (
     <div className={cn('flex min-h-full w-full justify-center bg-[#191919] py-10')}>
       <div className={cn('flex w-200 flex-col gap-[2.19rem]')}>
-        <div>
-          <h1
-            className={cn('text-[2.25rem] leading-[1.2] font-bold tracking-[-0.045rem] text-white')}
-          >
-            프로젝트 등록
-          </h1>
-        </div>
+        <HeroSection title="프로젝트 등록" />
         <RegisterProjectForm />
       </div>
     </div>
