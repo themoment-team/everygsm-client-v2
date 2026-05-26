0e0659030a52715c15610d481cdb20056064fa91
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2026-05-26 17:53:07 +0900

    refactor: 훅 스타일 통일성 있도록 수정

--- src/entities/user/model/useGetUsersSearch.ts
+++ src/entities/user/model/useGetUsersSearch.ts
@@ -4,10 +4,9 @@ import { get, userQueryKeys, userUrl } from '@/shared/api';
 
 import { UserSearchResponseType } from './types';
 
-export const useGetUsersSearch = (name: string) => {
-  return useQuery({
+export const useGetUsersSearch = (name: string) =>
+  useQuery({
     queryKey: userQueryKeys.getUsersSearch(name),
     queryFn: () => get<UserSearchResponseType>(userUrl.getUsersSearch(name)),
     enabled: !!name,
   });
-};
